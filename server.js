fbb318b0f6bedba210b02502643d8a22a26363a4
Osamagioghomwenwi Oghagbon <helloraaymii@gmail.com>
2023-11-29 23:00:13 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const xCall = require("../xcall");
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+
+app.post("/ci-cd", (req, res) => {
+  const { project, action, blockchain } = req.body;
+
+  // Simplified logic: Use xCall to interact with the blockchain
+  const result = xCall.executeAction(project, action, blockchain);
+
+  res.json({ success: result });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ xcall.js
@@ -0,0 +1,14 @@
+// xcall.js
+class xCall {
+  static executeAction(project, action, blockchain) {
+    // Simplified xCall logic
+    console.log(
+      `Executing action ${action} for project ${project} on blockchain ${blockchain}`
+    );
+
+    // Return a success status for simplicity
+    return true;
+  }
+}
+
+module.exports = xCall;
